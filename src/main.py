18adf3cd6758e945af8aabc50c911f5392ac6f7f
dmitry-blind-pew <dmitry.ol.bat@gmail.com>
2026-04-07 17:09:28 +0300

    initial commit

---
+++ src/main.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+import sys
+
+sys.path.append(str(Path(__file__).parent.parent))
+
+from fastapi import FastAPI
+import uvicorn
+
+
+app = FastAPI()
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
